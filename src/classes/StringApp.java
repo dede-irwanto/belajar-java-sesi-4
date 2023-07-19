e2d58a416ac630abe4e527f295283e319a9e12b2
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-19 23:47:26 +0800

    First commit

---
+++ src/classes/StringApp.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name = "Dede Irwanto";
+        String nameLowerCase = name.toLowerCase();
+        String nameUpperCase = name.toUpperCase();
+
+        System.out.println(name);
+        System.out.println(nameLowerCase);
+        System.out.println(nameUpperCase);
+        System.out.println(name.length());
+        System.out.println(name.startsWith("Dede"));
+        System.out.println(name.endsWith("Irwanto"));
+
+        String[] names = name.split(" ");
+        for (var value : names) {
+            System.out.println(value);
+        }
+    }
+}
